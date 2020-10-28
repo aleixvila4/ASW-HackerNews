cd7dcd2728e3366c172ed7277fe0097139845893
Ubuntu <sufang.zhou@estudiantat.upc.edu>
2020-10-28 02:22:44 +0000

    creo q solo falta los detalles

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,9 +1,10 @@
 class ContributionsController < ApplicationController
   before_action :set_contribution, only: [:show, :edit, :update, :destroy, :points,:link]
+  #:link -->no hace nada, lo puse para hacer una cosa de link pero no me ha salido
   # GET /contributions
   # GET /contributions.json
   def index
-    @contributions = Contribution.all
+    @contributions = Contribution.all.order("points DESC")
   end
   
    def link
